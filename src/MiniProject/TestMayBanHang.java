abbf86a2d534dcc9d881dc8dfed54833ccd8686d
nguyenvietanh1409 <vietanh20030914@gmail.com>
2022-04-27 16:14:13 +0700

    Test

---
+++ src/MiniProject/TestMayBanHang.java
@@ -0,0 +1,33 @@
+package MiniProject;
+
+import MiniProject.MayBanHangTuDong;
+
+import java.util.Scanner;
+public class TestMayBanHang {
+    public static void main(String[] args){
+        int chonSP;
+        MiniProject.MayBanHangTuDong sanPham1,sanPham2,sanPham3;
+        sanPham1 = new MiniProject.MayBanHangTuDong(1,"Sua Milo",10000F,11);
+        sanPham2 = new MiniProject.MayBanHangTuDong(2,"Bo huc", 15000F,24);
+        sanPham3 = new MayBanHangTuDong(3,"Vodka",1234567,1);
+        System.out.println("");
+        Scanner ip = new Scanner(System.in);
+        System.out.println("Chon san pham: ");
+        chonSP = ip.nextInt();
+        if (chonSP == 1){
+            sanPham1.soLuong();
+            sanPham1.nhanTien();
+            sanPham1.bill();
+        }
+        if (chonSP == 2){
+            sanPham2.soLuong();
+            sanPham2.nhanTien();
+            sanPham2.bill();
+        }
+        if (chonSP == 3){
+            sanPham2.soLuong();
+            sanPham3.nhanTien();
+            sanPham3.bill();
+        }
+    }
+}
